c9ebccf6a99ab9990680e9a9a46e2d1de2963902
David Kenzik <david@kenzik.com>
2015-04-07 17:07:46 -0400

    moved all FB functionality to promises; started on menu work (couple todos)

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ var _ = require('lodash');
 var restify = require('restify');
 var moment = require('moment');
 var uslug = require('uslug');
+var Q = require('q');
 
 // Setup Firebase
 var FB = new Firebase(config.webhook.firebase + '/buckets/' + config.webhook.siteName + '/' + config.webhook.secretKey + '/dev');
 
 // Content types
-var contentTypes = [];
+var contentTypes = false;
 
 // Login to Firebase
 console.log('Connecting to: ' + config.webhook.firebase + '/buckets/' + config.webhook.siteName + '/' + config.webhook.secretKey + '/dev');
@@ -31,29 +32,39 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
+
+// Seed content types
+getContentTypes().then(
+  function(data) {
+    contentTypes = data;
+  }
+);
+
 // Setup routes
 // ------------
 
 // Return all content types as array
 server.get('/content-types', function(req, res, next) {
-  FB.child('contentType').once('value', function(s) {
-    contentTypes= s.val();
-    res.send(200,contentTypes);
-  }, function(e) {
-    res.send(500,e);
-  });
+  console.log("In /content-types");
+  getContentTypes().then(
+    function(data) {
+      contentTypes = data;
+      res.send(200,contentTypes);
+    }, function(err) {
+      res.send(500,error);
+    }
+  );
   return next();
 });
-
+ 
 server.get('/menu', function(req, res, next) {
-  FB.child('data/menus').once('value', function(s) {
 
-    // TODO: Process menus, lookup related pages, etc.
-
-    res.send(200,s.val());
-
+  getMenus().then(function(data) {
+    res.send(200,data);
+  }, function(err) {
+    res.send(500,error);
   });
-  return next();
+
 });
 
 // Get all content type entries as array: /content-type/foo
@@ -63,81 +74,33 @@ server.get('/content-type/:type', function(req,res,next) {
 
   var contentType = req.params.type;
   var slug = req.query.slug || false;
+  var id = req.query.id || false;
 
-  // Get current content-types
-  FB.child('contentType').once('value', function(s) {
+  if(slug || id) {
+    getEntry(contentType,slug,id).then(
+      function(data) {
+        res.send(200,data);
+      }, function(err) {
+        res.send(404,"Not Found: " + slug);
+      }
+    );
+  } 
+  else if(!slug && !id) {
+    getEntries(contentType).then(
+      function(data) {
+        res.send(200,data);
+      }, function(err) {
+        res.send(404,"Not Found: " + contentType);
+      }
+    );
+  }
+  else {
+    res.send(500,"Server Error");
+  }
 
-    contentTypes = s.val();
-    if(_.keys(contentTypes).indexOf(contentType) == -1) {
-      res.send(404,'Content Type Not Found: ' + contentType );
-    } else {
-
-      var entry={};
-      var entryLocated = false;
-      var entries=[];
-
-      FB.child('data/' + contentType).once('value', function(s) {
-
-        if(slug) {
-          _.forEach(s.val(), function(n, i) {
-            
-            // Clean things up a bit
-            entry = processContentEntry(n,i,contentType);
-
-            // We found a slug that matches the request, send it back
-            if(entry.slug === slug) {
-              entryLocated = true;
-              res.send(200,entry);
-            } 
-          });
-
-          if(!entryLocated) {
-            res.send(404, 'Page Not Found: ' + slug);
-          }
-
-        } else if (req.query.id) {
-
-          _.forEach(s.val(), function(n, i) {
-
-              if(i === req.query.id) {
-                entryLocated = true;
-                res.send(200,processContentEntry(n,i,contentType));
-              }
-          });
-
-          if(!entryLocated) {
-            res.send(404, 'Page Not Found: ' + slug);
-          }
-
-        }
-        else {
-          // Deny any other params not defined
-          if(s.val() && !entryLocated) {
-            // TODO: Ignore any unrecognized queries and send back all entries
-            // For now, just send back all records
-
-            // Clean things up a bit
-            _.forEach(s.val(), function(n, i) { 
-              entries.push(processContentEntry(n,i,contentType));
-            });
-
-            res.send(200, entries);
-          } else {
-            res.send(404, 'Page Not Found');
-          }
-        }
-      }, function(e) {
-        res.send(500,'Error accessing "' + contentType + '".');
-      });
-    }
-
-
-  }, function(e) {
-    // Catch error
-  });
-
-  return next();
+  return next;
 });
+
 
 // Listen
 server.listen(config.server.port, function() {
@@ -172,10 +135,16 @@ function processContentEntry(entry,id,contentType) {
   return entry;
 }
 
-function getEntry(req,contentType) {
+function processMenu(menu,id) {
+  menu['_id']=id;
+
+  // TODO: Populate children
+
+  // TODO: Find page for creating URL
+
+  return menu;
 
 }
-
 
 // Functionality from webhook-cms
 // https://github.com/webhook/webhook-cms/issues/225
@@ -231,3 +200,150 @@ function parseCustomUrl (url, object, type) {
   url = url.replace(/#(\w)/g, replacer);
   return url;
 }
+
+// Meat
+//
+function getContentTypes() {
+
+  var deferred = Q.defer();
+  if(contentTypes.length > 0) deferred.resolve(contentTypes); 
+  FB.child('contentType').on('value', function(s) {
+    contentTypes = s.val();
+    deferred.resolve(contentTypes);
+  }, function(e) {
+    deferred.reject(e);
+  });
+
+  return deferred.promise;
+}
+
+function getEntries(contentType) {
+  var entries = [];
+
+  var deferred = Q.defer();
+  
+  if(!contentType) {
+    deferred.reject("You must provide the content type");
+  }
+
+  // This content type in our list?
+  // TODO: Might be nice to not rely on indexOf, since 'page' would match 'Home Page', 'Pages', etc.
+  if(_.keys(contentTypes).indexOf(contentType) == -1) {
+    deferred.reject('Content type not found: ' + contentType);
+  }  
+
+  FB.child('data/' + contentType).once('value', function(s) {
+    _.forEach(s.val(), function(n,i) {
+      processing(i);
+      entries.push(processContentEntry(n,i,contentType));
+    });
+    deferred.resolve(entries);
+  }, function(e) {
+    deferred.reject(e);
+  });  
+
+  return deferred.promise;  
+}
+
+function getMenus() {
+
+  var deferred = Q.defer();
+  var menus=[];
+
+  FB.child('data/menus').on('value', function(s) {
+    _.forEach(s.val(), function(n,i) {
+      menus.push(processMenu(n,i));
+    });
+    deferred.resolve(menus);
+  }, function(e) {
+    deferred.reject(e);
+  });
+
+  return deferred.promise;
+}
+
+function getMenu(id) {
+
+  var deferred = Q.defer();
+
+  // Need some parameters
+  if(!id) {
+    deferred.reject("Invalid parameters. You must provide a slug or id");
+  }
+
+  FB.child('data/menus/' + id).once('value', function(s) {
+    deferred.resolve(processMenu(s.val()));
+  }, function(e) {
+    deferred.reject(e);
+  });
+
+  return deferred.promise;
+
+}
+
+
+function getEntry(contentType, slug, id) {
+
+  var deferred = Q.defer();
+
+  // Need some parameters
+  if(!contentType) {
+    deferred.reject("You must provide the content type");
+  }
+  // Need some parameters
+  if(!slug && !id) {
+    deferred.reject("Invalid parameters. You must provide a slug or id");
+  }
+  // This content type in our list?
+  // TODO: Might be nice to not rely on indexOf, since 'page' would match 'Home Page', 'Pages', etc.
+  if(_.keys(contentTypes).indexOf(contentType) == -1) {
+    deferred.reject('Content type not found: ' + contentType);
+  }
+
+  // We have not found the entry 
+  var entryLocated = false;
+
+  // id takes priority
+  if(id) {
+    FB.child('data/' + contentType + '/' + id).once('value', function(s) {
+      entryLocated = true;
+      deferred.resolve(processContentEntry(s.val()));
+    }, function(e) {
+      deferred.reject(e);
+    });
+  }
+  // then slug
+  else if(slug) {
+    var entry={};
+    FB.child('data/' + contentType).once('value', function(s) {
+      _.forEach(s.val(), function(n, i) {
+        
+        // Clean things up a bit
+        entry = processContentEntry(n,i,contentType);
+
+        // We found a slug that matches the request, send it back
+        if(entry.slug === slug) {
+          entryLocated = true;
+          deferred.resolve(entry);
+        } 
+      });
+
+      // The forEach loop was not fruitful
+      if(!entryLocated) {
+        deferred.reject('Not Found: ' + slug);
+      }
+
+    }, function(e) {
+      deferred.reject(e);
+    });
+  } else {
+      if(!entryLocated) {
+        deferred.reject('Not Found: ' + id ? id : slug);    
+      }
+  }
+
+  return deferred.promise;
+
+}
+
+
