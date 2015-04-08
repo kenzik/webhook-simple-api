ebc47993ccd5aaaff48d75d55272e8abe2d93658
David Kenzik <david@kenzik.com>
2015-04-08 13:57:18 -0400

    first round of menu functionality

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ var restify = require('restify');
 var moment = require('moment');
 var uslug = require('uslug');
 var Q = require('q');
+var extend = require('util')._extend;
+var util = require('util');
 
 // Setup Firebase
 var FB = new Firebase(config.webhook.firebase + '/buckets/' + config.webhook.siteName + '/' + config.webhook.secretKey + '/dev');
 
-// Content types
-var contentTypes = false;
+
 
 // Login to Firebase
 console.log('Connecting to: ' + config.webhook.firebase + '/buckets/' + config.webhook.siteName + '/' + config.webhook.secretKey + '/dev');
@@ -34,26 +35,48 @@ server.use(restify.bodyParser());
 
 
 // Seed content types
-getContentTypes().then(
-  function(data) {
-    contentTypes = data;
-  }
-);
+var contentTypes = false;
+FB.child('contentType').on('value', function(s) {
+  contentTypes = s.val();
+});
+
+
+// Menu stuff
+var structuredMenus=[]; // structured with complete children structure
+var origMenus={}; // original structure from firebase with ID
+var strippedMenus={}; // original structure from firebase without children nodes
+
+// Seed/flatten menu
+FB.child('data/' + config.webhook.menusContentType).on('value', function(s) {
+  // Walk menus, store, dupe, strip children 
+  // 
+  _.forEach(s.val(),function(m,i) {
+
+    // Id
+    m['_id']=i;
+
+    // Keep a copy
+    origMenus[i] = m;;
+
+    // Get a copy
+    var s = extend({},m);
+
+    // delete children
+    if(s.children) delete s.children;
+
+    // Add to lookup for later
+    strippedMenus[i] = s;
+
+  });
+})
+
 
 // Setup routes
 // ------------
 
 // Return all content types as array
 server.get('/content-types', function(req, res, next) {
-  console.log("In /content-types");
-  getContentTypes().then(
-    function(data) {
-      contentTypes = data;
-      res.send(200,contentTypes);
-    }, function(err) {
-      res.send(500,error);
-    }
-  );
+  res.send(200,contentTypes);
   return next();
 });
  
@@ -64,9 +87,19 @@ server.get('/menu', function(req, res, next) {
   }, function(err) {
     res.send(500,error);
   });
-
+  return next;
 });
 
+server.get('/menu/:id', function(req, res, next) {
+
+  getMenu(req.params.id).then(function(data) {
+    res.send(200,data);
+  }, function(err) {
+    res.send(500,error);
+  });
+  return next;
+
+});
 // Get all content type entries as array: /content-type/foo
 // Get a content type entry as object by slug /content-type/foo?slug=bar
 // Get a content type entry as object by FB key id /content-type/foo?id=-FJfkfjjf234r334fzznFF-
@@ -102,115 +135,15 @@ server.get('/content-type/:type', function(req,res,next) {
 });
 
 
+// End Routes
+
 // Listen
 server.listen(config.server.port, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
 
 
-// Utils
-//
-function fbAuthHandler(err,authData) {
-  if(err) {
-    console.log(err);
-    process.exit();
-  } else {    
-    console.log('Connected. Firebase authentication successful as ' + config.webhook.username);
-  }
-}
 
-function processContentEntry(entry,id,contentType) {  
-  // console.log(page);
-  if(!entry) return false;
-  entry['_id']=id;
-
-  if(!entry.slug) {
-    var entrySlug = slugger({
-      name: entry.name,
-      publish_date: moment(entry.publish_date)
-    }, contentType, contentTypes[contentType].customUrls ? contentTypes[contentType].customUrls : null);
-    entry['slug']=entrySlug.substring(entrySlug.indexOf('/') + 1);
-  }
-
-  return entry;
-}
-
-function processMenu(menu,id) {
-  menu['_id']=id;
-
-  // TODO: Populate children
-  if(menu.children) {
-    var children = [];
-    _.forEach(menu.children, function(m,i) {
-      children.push(processMenu(m.split(' ')[1]));
-    });
-    menu.children=children;
-  }
-
-  // TODO: Find page for creating URL
-
-  return menu;
-
-}
-
-function processMenuChildren(children) {
-  var children = [];
-}
-
-// Functionality from webhook-cms
-// https://github.com/webhook/webhook-cms/issues/225
-//
-function slugger(item, type, customUrls) {
-  var tmpSlug = '';
-  tmpSlug = uslug(item.name).toLowerCase();
-
-  if(customUrls && customUrls.individualUrl) {
-    tmpSlug = parseCustomUrl(customUrls.individualUrl, item, type) + '/' + tmpSlug;
-  }
-
-  if(customUrls && customUrls.listUrl) {
-    tmpSlug = customUrls.listUrl + '/' + tmpSlug;
-  } else {
-    tmpSlug = type + '/' + tmpSlug;
-  }
-  return tmpSlug;
-}
-
-// Functionality from webhook-cms
-// https://github.com/webhook/webhook-cms/issues/225
-//
-function parseCustomUrl (url, object, type) {
-  var publishDate = object.publish_date ? object.publish_date : object;
-
-  publishDate = moment(publishDate);
-
-  function replacer(match, timeIdent, offset, string){
-    if(timeIdent === 'Y') {
-      return publishDate.format('YYYY').toLowerCase();
-    } else if (timeIdent === 'y') {
-      return publishDate.format('YY').toLowerCase();
-    } else if (timeIdent === 'm') {
-      return publishDate.format('MM').toLowerCase();
-    } else if (timeIdent === 'n') {
-      return publishDate.format('M').toLowerCase();
-    } else if (timeIdent === 'F') {
-      return publishDate.format('MMMM').toLowerCase();
-    } else if (timeIdent === 'M') {
-      return publishDate.format('MMM').toLowerCase();
-    } else if (timeIdent === 'd') {
-      return publishDate.format('DD').toLowerCase();
-    } else if (timeIdent === 'j') {
-      return publishDate.format('D').toLowerCase();
-    } else if (timeIdent === 'T') {
-      return type.toLowerCase();
-    } else {
-      return match;
-    }
-  }
-
-  url = url.replace(/#(\w)/g, replacer);
-  return url;
-}
 
 // Meat
 //
@@ -245,7 +178,7 @@ function getEntries(contentType) {
 
   FB.child('data/' + contentType).once('value', function(s) {
     _.forEach(s.val(), function(n,i) {
-      processing(i);
+      //processing(i);
       entries.push(processContentEntry(n,i,contentType));
     });
     deferred.resolve(entries);
@@ -257,41 +190,41 @@ function getEntries(contentType) {
 }
 
 function getMenus() {
-
+  // TODO: Add/process page for Urls
   var deferred = Q.defer();
-  var menus=[];
-
-  FB.child('data/menus').on('value', function(s) {
-    _.forEach(s.val(), function(n,i) {
-      menus.push(processMenu(n,i));
+  if(structuredMenus.length > 0) {
+    deferred.resolve(structuredMenus);
+  } else {
+    _.forEach(origMenus, function(menu, mi) {
+      
+      if(menu.level === 'First') {
+        console.log("Level 1 Processing: " + menu.name + ' - ' + menu['_id']);
+        structuredMenus.push(replaceChildren(menu));
+      }
     });
-    deferred.resolve(menus);
-  }, function(e) {
-    deferred.reject(e);
-  });
-
-  return deferred.promise;
-}
-
-function getMenu(id) {
-
-  var deferred = Q.defer();
-
-  // Need some parameters
-  if(!id) {
-    deferred.reject("Invalid parameters. You must provide a slug or id");
+    deferred.resolve(structuredMenus);
   }
-
-  FB.child('data/menus/' + id).once('value', function(s) {
-    deferred.resolve(processMenu(s.val()));
-  }, function(e) {
-    deferred.reject(e);
-  });
-
   return deferred.promise;
-
 }
 
+function replaceChildren(menu) {
+  var s = extend({}, menu);
+  delete s.children;
+
+  if(menu.children) {
+    s.children=[];
+    console.log("-- Found Children: ", menu.children);
+    _.forEach(menu.children, function(child, ci) {
+      var childId = child.split(' ')[1];
+      console.log("---- Comparing " + childId + "to parent: " + menu['_id']);
+      // Level 2 - That's as far as we can go.
+      if(childId !== menu['_id']) {
+        s.children.push(strippedMenus[childId]);
+      }
+    });
+  }  
+  return s;
+}
 
 function getEntry(contentType, slug, id) {
 
@@ -355,6 +288,90 @@ function getEntry(contentType, slug, id) {
 
   return deferred.promise;
 
+}
+
+
+// Utils
+//
+function fbAuthHandler(err,authData) {
+  if(err) {
+    console.log(err);
+    process.exit();
+  } else {    
+    console.log('Connected. Firebase authentication successful as ' + config.webhook.username);
+  }
+}
+
+function processContentEntry(entry,id,contentType) {  
+  // console.log(page);
+  if(!entry) return false;
+  entry['_id']=id;
+
+  if(!entry.slug) {
+    var entrySlug = slugger({
+      name: entry.name,
+      publish_date: moment(entry.publish_date)
+    }, contentType, contentTypes[contentType].customUrls ? contentTypes[contentType].customUrls : null);
+    entry['slug']=entrySlug.substring(entrySlug.indexOf('/') + 1);
+  }
+
+  return entry;
+}
+
+
+// Functionality from webhook-cms
+// https://github.com/webhook/webhook-cms/issues/225
+//
+function slugger(item, type, customUrls) {
+  var tmpSlug = '';
+  tmpSlug = uslug(item.name).toLowerCase();
+
+  if(customUrls && customUrls.individualUrl) {
+    tmpSlug = parseCustomUrl(customUrls.individualUrl, item, type) + '/' + tmpSlug;
+  }
+
+  if(customUrls && customUrls.listUrl) {
+    tmpSlug = customUrls.listUrl + '/' + tmpSlug;
+  } else {
+    tmpSlug = type + '/' + tmpSlug;
+  }
+  return tmpSlug;
+}
+
+// Functionality from webhook-cms
+// https://github.com/webhook/webhook-cms/issues/225
+//
+function parseCustomUrl (url, object, type) {
+  var publishDate = object.publish_date ? object.publish_date : object;
+
+  publishDate = moment(publishDate);
+
+  function replacer(match, timeIdent, offset, string){
+    if(timeIdent === 'Y') {
+      return publishDate.format('YYYY').toLowerCase();
+    } else if (timeIdent === 'y') {
+      return publishDate.format('YY').toLowerCase();
+    } else if (timeIdent === 'm') {
+      return publishDate.format('MM').toLowerCase();
+    } else if (timeIdent === 'n') {
+      return publishDate.format('M').toLowerCase();
+    } else if (timeIdent === 'F') {
+      return publishDate.format('MMMM').toLowerCase();
+    } else if (timeIdent === 'M') {
+      return publishDate.format('MMM').toLowerCase();
+    } else if (timeIdent === 'd') {
+      return publishDate.format('DD').toLowerCase();
+    } else if (timeIdent === 'j') {
+      return publishDate.format('D').toLowerCase();
+    } else if (timeIdent === 'T') {
+      return type.toLowerCase();
+    } else {
+      return match;
+    }
+  }
+
+  url = url.replace(/#(\w)/g, replacer);
+  return url;
 }
 
 
